efbc4eb06b57b62439720132875b3c4d36f6c0f3
nuclearMonkeys <lyndond@uci.edu>
2019-08-02 22:19:31 -0700

    Added Weird Beez Behavior

--- Scripts/BasicEnemy.gd
+++ Scripts/BasicEnemy.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
-const GRAVITY: float = 600.0;
-const JUMP_FORCE: float = 300.0;
-var WALK_SPEED: int = -100;
+const GRAVITY: float = 600.0
+const JUMP_FORCE: float = 300.0
+var WALK_SPEED: int = -100
+var temp_speed: int = 0
 
 var velocity: Vector2 = Vector2.ZERO;
 var jumping = false;
@@ -18,9 +19,33 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector2(0, 1))
 
 
-func _on_Area2D_area_entered(area: Area2D) -> void:
-	if area.is_in_group("Torch"):
-		var temp_speed: int = WALK_SPEED;
-		WALK_SPEED = 0;
-		yield(get_tree().create_timer(3.0), "timeout")
+func _on_PlayerAreaVicinityFront_body_shape_exited(body_id, body, body_shape, area_shape):
+	if body.name == "Player":
 		WALK_SPEED = temp_speed;
+		temp_speed = 0;
+
+
+func _on_PlayerAreaVicinityBack_body_shape_exited(body_id, body, body_shape, area_shape):
+	if body.name == "Player":
+		WALK_SPEED = temp_speed;
+		temp_speed = 0;
+
+
+func _on_PlayerAreaVicinityFront_body_shape_entered(body_id, body, body_shape, area_shape):
+	if body.name == "Player":
+		temp_speed = WALK_SPEED;
+		WALK_SPEED = 300
+		yield(get_tree().create_timer(3.0), "timeout")
+		print("wait man");
+		WALK_SPEED = temp_speed
+		
+func _on_PlayerAreaVicinityBack_body_shape_entered(body_id, body, body_shape, area_shape):
+	if body.name == "Player":
+		temp_speed = WALK_SPEED;
+		WALK_SPEED = -300
+
+		
+
+
+func _on_PlayerAreaVicinityFront_body_exited(body):
+	pass # Replace with function body.
