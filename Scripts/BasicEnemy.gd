47616ecf3eb15551fe3ff7fea784a848f99b47ec
Duncan Sparks <duncan.sprks@gmail.com>
2019-08-03 16:17:18 -0700

    idfk

--- Scripts/BasicEnemy.gd
+++ Scripts/BasicEnemy.gd
@@ -8,32 +8,42 @@ var temp_speed: int = 0
 var velocity: Vector2 = Vector2.ZERO;
 var jumping = false;
 
+onready var anim_player := $AnimationPlayer
+
 func _ready() -> void:
 	pass
 
 func _physics_process(delta):
 	if !$RayCast2D.is_colliding() or is_on_wall():
 		WALK_SPEED *= -1;
-	velocity.x = WALK_SPEED;
+		if sign(WALK_SPEED) == 1:
+			anim_player.play("WalkRight")
+			#anim_player.current_animation_position = 2.2
+			anim_player.set_speed_scale(-2.5)
+		elif sign(WALK_SPEED) == -1:
+			anim_player.play("WalkLeft")
+			#anim_player.current_animation_position = 2.2
+			anim_player.set_speed_scale(-2.5)
+	velocity.x = WALK_SPEED
 	
 	velocity = move_and_slide(velocity, Vector2(0, 1))
 
 
 func _on_PlayerAreaVicinityFront_body_shape_exited(body_id, body, body_shape, area_shape):
 	if body.name == "Player":
-		WALK_SPEED = temp_speed;
-		temp_speed = 0;
+		WALK_SPEED = temp_speed
+		temp_speed = 0
 
 
 func _on_PlayerAreaVicinityBack_body_shape_exited(body_id, body, body_shape, area_shape):
 	if body.name == "Player":
-		WALK_SPEED = temp_speed;
-		temp_speed = 0;
+		WALK_SPEED = temp_speed
+		temp_speed = 0
 
 
 func _on_PlayerAreaVicinityFront_body_shape_entered(body_id, body, body_shape, area_shape):
 	if body.name == "Player":
-		temp_speed = WALK_SPEED;
+		temp_speed = WALK_SPEED
 		WALK_SPEED = 300
 		yield(get_tree().create_timer(3.0), "timeout")
 		print("wait man");
@@ -41,7 +51,7 @@ func _on_PlayerAreaVicinityFront_body_shape_entered(body_id, body, body_shape, a
 		
 func _on_PlayerAreaVicinityBack_body_shape_entered(body_id, body, body_shape, area_shape):
 	if body.name == "Player":
-		temp_speed = WALK_SPEED;
+		temp_speed = WALK_SPEED
 		WALK_SPEED = -300
 
 func _on_HitArea_area_entered(area):
