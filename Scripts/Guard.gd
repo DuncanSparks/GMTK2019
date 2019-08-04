d50eeeaf155012a0c4e597a49ae8ccbd23ee4d7e
Duncan Sparks <duncan.sprks@gmail.com>
2019-08-03 22:20:39 -0700

    Fix guard behavior

--- Scripts/Guard.gd
+++ Scripts/Guard.gd
@@ -17,10 +17,7 @@ func _ready():
 	throwableObject = get_tree().get_root().get_node("Scene").get_node("ThrowableObject")
 
 func _physics_process(delta):
-	if throwableObject.player_in_area:
-		state = State.STOP
-	else:
-		state = State.MOVE
+	state = State.STOP if throwableObject.held else State.MOVE
 	
 	match state:
 		State.MOVE:
@@ -30,7 +27,18 @@ func _physics_process(delta):
 			
 	direction = move_and_slide(direction, player.position)
 	
-	anim_player.set_assigned_animation("Left" if direction.x < 0 else "Right")
+	#anim_player.set_assigned_animation("Left" if direction.x < 0 else "Right")
+	
+	if direction.x < 0:
+		anim_player.set_assigned_animation("Left")
+	elif direction.x > 0:
+		anim_player.set_assigned_animation("Right")
 
-func _on_KillArea_area_entered(area):
-	print()
+#func _on_KillArea_area_entered(area):
+	#print("TEST")
+	#get_tree().reload_current_scene()
+
+
+func _on_KillArea_body_entered(body: PhysicsBody2D) -> void:
+	if body != null and body.is_in_group("Player"):
+		get_tree().reload_current_scene()
